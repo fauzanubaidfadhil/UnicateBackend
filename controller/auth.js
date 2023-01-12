c198e8dcfe04ed9809ea8c9648f6ff0850a76760
Fauzan Ubaid Fadhil <fauzanubaidfadhil@gmail.com>
2023-01-12 22:21:30 +0700

    add jwt, user login and login validation

--- controller/auth.js
+++ controller/auth.js
@@ -1,6 +1,8 @@
+require("dotenv").config();
 const bcrypt = require("bcryptjs");
 const { query } = require("../database/config");
 const { date } = require("../utils/tools");
+const jwt = require("jsonwebtoken");
 
 async function regisValidation(req, res, next) {
   let errorMessage = "";
@@ -42,7 +44,7 @@ async function userRegis(req, res) {
     const salt = await bcrypt.genSalt(12);
     const hash = await bcrypt.hash(password, salt);
 
-    const userData = await query(`
+    await query(`
         INSERT INTO users (
             username, fullname, email, phonenumber, password, created_by, created_at, updated_by, updated_at
         ) VALUES (
@@ -51,16 +53,94 @@ async function userRegis(req, res) {
     `);
 
     const [{ id }] = await query(` SELECT MAX(id) as id FROM users;`);
-    console.log(id);
 
-    //
+    await query(`
+      UPDATE users SET
+      created_by = ${id},
+      updated_by = ${id}
+      WHERE id = ${id};
+    `);
 
-    return res.status(200).json({ message: "Register successfully!" });
+    const [userData] = await query(`
+      SELECT id, username, fullname, email FROM users WHERE id = ${id} 
+    `);
+
+    jwt.sign(userData, process.env.privateKey, (err, token) => {
+      if (err) throw err;
+
+      return res.status(200).json({
+        message: "Register successfully!",
+        Authorization: `bearer ${token}`,
+      });
+    });
   } catch (error) {
     return res.status(400).json({ message: "Something went wrong!" });
   }
 }
 
 // login dan validattion login
+async function validationLogin(req, res, next) {
+  const { email, password } = req.body;
+  try {
+    if (email === undefined)
+      return res.status(400).json({ message: "Email required!" });
+    if (password === undefined)
+      return res.status(400).json({ message: "Password required!" });
 
-module.exports = { userRegis, regisValidation };
+    // next is used for go to the next middleware
+    next();
+  } catch (error) {
+    return res.status(400).json({ message: "Something went wrong!" });
+  }
+}
+
+async function userLogin(req, res) {
+  const { email, password } = req.body;
+  try {
+    const [checkUser] = await query(`
+      SELECT id, password FROM users WHERE email = '${email}';
+    `);
+
+    if (checkUser === undefined)
+      return res.status(400).json({ message: "User not exists!" });
+
+    const matchPassword = await bcrypt.compare(password, checkUser.password);
+    if (!matchPassword)
+      return res.status(400).json({ message: "Invalid email/password!" });
+
+    const [userData] = await query(`
+      SELECT id, username, fullname, email FROM users WHERE id = ${checkUser.id} 
+    `);
+
+    jwt.sign(userData, process.env.privateKey, (err, token) => {
+      if (err) throw err;
+
+      return res.status(200).json({
+        Authorization: `bearer ${token}`,
+      });
+    });
+  } catch (error) {
+    return res.status(400).json({ message: "Something went wrong!" });
+  }
+}
+
+module.exports = { userRegis, regisValidation, validationLogin, userLogin };
+
+// const token = jwt.sign({ email: result.email, id: result._id }, SECRET_KEY);
+
+// // username already taken
+// User.findOne({ username: username }, (err, user) => {
+//   if (err) {
+//     return res.status(500).json({ message: "Error on the server" });
+//   }
+//   if (user) {
+//     return res.status(409).json({ message: "Username already exist" });
+//   }
+
+//   // send token
+//   const payload = {
+//     username,
+//   };
+//   const token = jwt.sign(payload, secretKey);
+//   res.status(200).json({ token });
+// });
--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,13 @@
 const express = require("express");
-const { regisValidation, userRegis } = require("../controller/auth");
+const {
+  regisValidation,
+  userRegis,
+  validationLogin,
+  userLogin,
+} = require("../controller/auth");
 const routes = express();
 
 routes.post("/user-register", [regisValidation, userRegis]);
+routes.post("/user-login", [validationLogin, userLogin]);
 
 module.exports = routes;
